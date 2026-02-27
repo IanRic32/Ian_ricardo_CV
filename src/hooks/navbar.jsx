aa6712d1aa8ef48b342e5fbbdb14d035e85236c0
IanRic32 <riosianric@gmail.com>
2026-02-27 14:56:01 -0600

    actualiza enlaces en el navbar y mejora estilos responsivos

--- src/hooks/navbar.jsx
+++ src/hooks/navbar.jsx
@@ -20,7 +20,7 @@ function Navegacion() {
                 <li><a href="https://github.com/IanRic32?tab=repositories" target="_blank" rel="noreferrer"><FaGithub /></a></li>
                 <li><a href="https://mercartfront.vercel.app/" target="_blank" rel="noreferrer">MERCART</a></li>
                 <li><a href="https://testigos-haleon.vercel.app/" target="_blank" rel="noreferrer">Haleon</a></li>
-                <li><a href="#kaggle"><SiKaggle /></a></li>
+                <li><a href="https://www.kaggle.com/rivi03"><SiKaggle /></a></li>
                 <li><a href="https://www.linkedin.com/in/ian-ricardo-rios-velazquez" target="_blank" rel="noreferrer"><FaLinkedin /></a></li>
             </ul>
         </nav>
